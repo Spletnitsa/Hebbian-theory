17777f775166d8087537ef48e0d75ade534d9898
Spletnitsa <hotfrogsdaddy@gmail.com>
2022-12-06 20:55:32 +0300

    Доработал метод LearningNeuron, попытался исправить неявное использование полей класса в методе ActivateFunction.

--- Hebbian theory/Program.cs
+++ Hebbian theory/Program.cs
@@ -8,16 +8,14 @@
         private double W1 = 0;
         private double W2 = 0;
         private double T = 0;
-        private const int N = 1;
-        private double NetRes;
         private int output;
 
-        public void Net() 
+        private double Net() 
         {
-            NetRes = W1 * X1 + W2 * X2 + T * X0;
+            return W1 * X1 + W2 * X2 + T * X0;
         }
 
-        public int ActivateFunction() 
+        private int ActivateFunction(double NetRes) 
         {
             if (NetRes > 0)
                 return 1;
@@ -27,27 +25,37 @@
                 return 0;
         }
 
-        public bool LearningNeuron(int X1, int X2,  int D)
+        public void LearningNeuron(int[] X1, int[] X2,  int[] D)
         {
-            this.X1 = X1;
-            this.X2 = X2;
-            bool out_is_correct = true;
-            Net();
-            output = ActivateFunction();
-            if (output != D)
+            bool exit;
+            int j = 0;
+            do
             {
-                W1 += this.X1 * D;
-                W2 += this.X2 * D;
-                T -= D;
-                Console.WriteLine($"Изменение весов:\n W1 = {W1}\n W2 = {W2}\n T = {T}");
-                out_is_correct = false;
-            }
-            else
-                Console.WriteLine("Выход совпал с эталонным значением");
+                j++;
+                Console.WriteLine($"Прогон {j}");
+                exit = true;
+                for (int i = 0; i < D.Length; i++)
+                {
+                    this.X1 = X1[i];
+                    this.X2 = X2[i];
 
-            Console.WriteLine($"Выход = {output}\nЭталонное значение = {D}\n");
+                    output = ActivateFunction(Net());
 
-            return out_is_correct;
+                    if (output != D[i])
+                    {
+                        exit = false;
+
+                        W1 += this.X1 * D[i];
+                        W2 += this.X2 * D[i];
+                        T -= D[i];
+                        Console.WriteLine($"Изменение весов:\n W1 = {W1}\n W2 = {W2}\n T = {T}");
+                    }
+                    else
+                        Console.WriteLine("Выход совпал с эталонным значением");
+
+                    Console.WriteLine($"Выход = {output}\nЭталонное значение = {D[i]}\n");
+                }
+            } while (!exit);
         }
     }
     internal class Program
@@ -58,21 +66,7 @@
             int[] input2 = new int[] { -1, -1, 1, 1 };
             int[] D = new int[] { -1, -1, -1, 1};
             neuron firstNeuron = new neuron();
-            bool exit;
-            int j = 0;
-            do
-            {
-                j++;
-                Console.WriteLine($"Прогон {j}");
-                exit = true;
-                for (int i = 0; i < D.Length; i++)
-                {
-                    if (firstNeuron.LearningNeuron(input1[i], input2[i], D[i]) == false)
-                    {
-                        exit = false;
-                    }
-                }
-            } while (!exit);
+            firstNeuron.LearningNeuron(input1, input2, D);
         }
     }
 }
